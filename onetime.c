68a14b24c980c8193e294af0708e18a5f0479211
GrantWishes <grant-gertsen@uiowa.edu>
2017-02-08 23:41:06 -0600

    Finished onetimepad

--- onetime.c
+++ onetime.c
@@ -27,7 +27,31 @@ int main(int argc, char* argv[]) {
 		fprintf(stderr, "First argument must be 'encrypt' or 'decrypt'!\n");
 		exit(1);
 	}
+
+	if(keyFile == NULL || inFile == NULL || outFile == NULL) {
+		fprintf(stderr, "Error opening file");
+		exit(1);
+	}
+
+	// you should/can read the file into memory one byte at a time to avoid memory issues, using the first 4 bytes
+	// to get the correct size.
+	int size;
+	unsigned char keyBuffer;
+	unsigned char inBuffer;
+	unsigned char output;
+	fread(&size,sizeof(int),1, keyFile); // eat the data
+	fread(&size,sizeof(int),1, inFile);  // get the same value as above (lol)
+	fwrite(&size,sizeof(int),1, outFile);
+	printf("Size of file is %d\n",size);
 	
+	for(int i = 0; i < size; i++) {
+		
+		fread(&keyBuffer, sizeof(unsigned char), 1, keyFile);
+		fread(&inBuffer,  sizeof(unsigned char), 1, inFile);
+		output = keyBuffer ^ inBuffer;
+		fwrite(&output, sizeof(unsigned char), 1, outFile);
+	}
+
 	fclose(keyFile);
 	fclose(inFile);
 	fclose(outFile);
